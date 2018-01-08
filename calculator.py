1cdd1c60ba861403b847918d68cbe528c7672cf4
Irene Solaiman <irs623@student.hks.harvard.edu>
2018-01-08 17:10:35 -0500

    add bonus calculator

--- calculator.py
+++ calculator.py
@@ -14,6 +14,17 @@ def subtract(a,b):
 def divide(a,b):
 	return a / b
 
+# This part of the code defines a function
+def square(a):
+	return a*a
+
+# This part of the code defines a function
+def cube(a):
+	return a*a*a
+
+# This part of the code defines a function
+def square_n_times(a,n):
+	return (a*a)*n
 
 print "I'm going use the calculator functions to multiply 5 and 6"
 x = multiply(5,6)
