c067980eb46a2017a20a9c83b91534e8d4f25c03
Irene Solaiman <irs623@student.hks.harvard.edu>
2018-01-08 17:05:33 -0500

    add print multiply line

--- calculator.py
+++ calculator.py
@@ -13,3 +13,8 @@ def subtract(a,b):
 # This part of the code defines a function
 def divide(a,b):
 	return a / b
+
+
+print "I'm going use the calculator functions to multiply 5 and 6"
+x = multiply(5,6)
+print x
